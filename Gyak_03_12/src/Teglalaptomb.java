bfa09492c5bff2bd31eb317c244e4ffcc4e08be9
juhasz46 <juhasz46@iit422.iit.uni-miskolc.hu>
2018-03-12 16:41:35 +0100

    03_12 Gyakorlat

--- Gyak_03_12/src/Teglalaptomb.java
+++ Gyak_03_12/src/Teglalaptomb.java
@@ -1,11 +1,22 @@
 
-/*public class Teglalaptomb {
-	public static void main(String[] args) {
-		int meret=10;
-		Teglalap[] Teglalaptomb []= new Teglalap[meret];
-		
-		for (int i = 0; i < Teglalaptomb.length; i++) {
-			teglalaptomb[i] =new Teglalap((int)(Math.random() * 9)+2, (int) Math.random() * 9)+2;
-		}
-	}
-}*/
+public class Teglalaptomb {
+    public static void main(String[] args) {
+
+        int meret = 10;
+        Teglalap[] teglalaptomb = new Teglalap[meret];
+
+        for (int i = 0; i < teglalaptomb.length; i++) {
+            teglalaptomb[i] = new Teglalap((int)(Math.random() *9) +2);
+            System.out.println(teglalaptomb[i]);
+        }
+        int min_teruletu_teglalap_indexe = 0;
+        for (int i = 0; i < teglalaptomb.length; i++) {
+            if (teglalaptomb[min_teruletu_teglalap_indexe].nagyobbe(teglalaptomb[i]))
+            min_teruletu_teglalap_indexe = i;
+        }
+        
+        System.out.println(teglalaptomb[min_teruletu_teglalap_indexe]);
+       
+
+    }
+}
