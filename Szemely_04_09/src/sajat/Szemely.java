6208a6ed24f6f772eb1076096e57a72a1c221cf6
juhasz46 <juhasz46@iit441.iit.uni-miskolc.hu>
2018-04-09 16:40:40 +0200

    Gyakorlat 2. feladat

---
+++ Szemely_04_09/src/sajat/Szemely.java
@@ -0,0 +1,29 @@
+package sajat;
+
+public class Szemely {
+	private String nev;
+	private int kor;	
+	
+	public Szemely(String nev, int kor) {
+		this.nev = nev;
+		this.kor = kor;
+	}
+
+	public String getNev() {
+		return nev;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	@Override
+	public String toString() {
+		return "Nev: " + getNev() + ", kor: " + getKor();
+	}
+	
+	public boolean eletkorNagyobbMint(Szemely masik_szemely) {
+		return getKor() >= masik_szemely.getKor();
+	}
+		
+}
